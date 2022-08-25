f18578e4d3b38310e1cd6df773e130f257bd3a20
TahsinSumiya <tahsinadity187@gmail.com>
2022-08-26 01:20:32 +0600

    usereducer

---
+++ src/BookList.js
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+const booksData=[
+    {id :1,name:"brida"},
+    {id :2,name:"The alchemist"},
+    {id :3,name:"TimeMachine"},
+    
+];
+const BookList = () => {
+    const [books,setBooks]=useState(booksData)
+  return (
+    <div>
+      ARAFATH
+    </div>
+  )
+}
+
+export default BookList
